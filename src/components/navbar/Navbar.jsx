4a54c311cfee1412c1f0ee5f18e05f4d07c031f0
ThomassMathisen <thomasmathisen@hotmail.com>
2023-11-07 18:58:56 -0300

    Navbar design and setup css files

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
-
+import styles from "./navbar.module.css"
 
 const links = [
   {
@@ -37,12 +39,13 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-        <Link href="/">Website</Link>
-    <div>
+    <div className={styles.container}>
+        <Link href="/" className={styles.logo}>Website</Link>
+    <div className={styles.links}>
       {links.map(link => (
-          <Link key={link.id} href={link.url}>{link.title}</Link>
+          <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
           ))}
+        <button className={styles.logout}>Logout</button>
     </div>
           </div>
   )
