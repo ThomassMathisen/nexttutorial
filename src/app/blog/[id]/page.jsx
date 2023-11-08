95501b1e6cbaf690a40084395393410e4a3ad21d
ThomassMathisen <thomasmathisen@hotmail.com>
2023-11-08 13:14:43 -0300

    Create blog design

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const BlogPost = () => {
   return (
-    <div>BlogPost</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ad
+            laudantium aspernatur quam suscipit, enim aperiam sit voluptas eius
+            perspiciatis repellat nulla reiciendis. Magni eveniet nostrum
+            tenetur beatae incidunt atque.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/14021938/pexels-photo-14021938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>John Doe</span>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/18046372/pexels-photo-18046372/free-photo-of-person-walking-on-road-in-desert-leading-towards-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam in
+          commodi assumenda reprehenderit, dicta omnis. Dignissimos, facere
+          reiciendis qui sint asperiores obcaecati neque distinctio quibusdam
+          quos, fugiat, ad libero ipsam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quibusdam in commodi assumenda reprehenderit, dicta
+          omnis. Dignissimos, facere reiciendis qui sint asperiores obcaecati
+          neque distinctio quibusdam quos, fugiat, ad libero ipsam! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Quibusdam in commodi
+          assumenda reprehenderit, dicta omnis. Dignissimos, facere reiciendis
+          qui sint asperiores obcaecati neque distinctio quibusdam quos, fugiat,
+          ad libero ipsam! Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Quibusdam in commodi assumenda reprehenderit, dicta omnis.
+          Dignissimos, facere reiciendis qui sint asperiores obcaecati neque
+          distinctio quibusdam quos, fugiat, ad libero ipsam!
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default BlogPost
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
-  )
-}
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testID" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/18940197/pexels-photo-18940197/free-photo-of-albino-cockatiel.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A quod
+            sequi consequatur? Commodi ad fugit sequi enim, architecto
+            reprehenderit quam odio, minus dolores, quaerat praesentium fugiat?
+            Porro eligendi voluptates soluta?
+          </p>
+        </div>
+      </Link>
+      <Link href="" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/18940197/pexels-photo-18940197/free-photo-of-albino-cockatiel.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A quod
+            sequi consequatur? Commodi ad fugit sequi enim, architecto
+            reprehenderit quam odio, minus dolores, quaerat praesentium fugiat?
+            Porro eligendi voluptates soluta?
+          </p>
+        </div>
+      </Link>
+      <Link href="" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/18940197/pexels-photo-18940197/free-photo-of-albino-cockatiel.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A quod
+            sequi consequatur? Commodi ad fugit sequi enim, architecto
+            reprehenderit quam odio, minus dolores, quaerat praesentium fugiat?
+            Porro eligendi voluptates soluta?
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
