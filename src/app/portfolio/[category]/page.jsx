83464b814083530b2f776f309a862a0a042eb4b7
ThomassMathisen <thomasmathisen@hotmail.com>
2023-11-08 12:57:01 -0300

    Create portfolio page design

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import Button from "@/components/Button/Button";
+import styles from "./page.module.css";
+import Image from "next/image";
+import React from "react";
 
-const Category = () => {
-return (
-    <div>Category</div>
-  )
-}
+const Category = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Creative Portfolio</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta,
+            sed eos voluptate doloremque dicta ipsa neque iusto, ab ipsum,
+            obcaecati eius. Cum porro, fugiat sint exercitationem culpa tempore
+            amet deserunt.
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/10990271/pexels-photo-10990271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Creative Portfolio</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta,
+            sed eos voluptate doloremque dicta ipsa neque iusto, ab ipsum,
+            obcaecati eius. Cum porro, fugiat sint exercitationem culpa tempore
+            amet deserunt.
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/10990271/pexels-photo-10990271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
